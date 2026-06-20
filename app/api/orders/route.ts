5467033b458f208cc540048c04e66c2f220b506b
caidenrev <cimicimicilung4868@gmail.com>
2026-06-20 13:16:22 +0700

    switch to production mode

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -164,7 +164,7 @@ async function createMidtransTransaction(order: Order): Promise<string | null> {
   try {
     const midtransClient = require('midtrans-client');
     const snap = new midtransClient.Snap({
-      isProduction: false,
+      isProduction: true,
       serverKey: process.env.MIDTRANS_SERVER_KEY || '',
       clientKey: process.env.MIDTRANS_CLIENT_KEY || ''
     });
